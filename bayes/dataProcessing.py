7f69d95acf112aefa7685a6bc9492a6721afbb6c
lopez-jose <lezep2@gmail.com>
2020-11-17 21:54:41 -0800

    added printout method for days and months

--- bayes/dataProcessing.py
+++ bayes/dataProcessing.py
@@ -1,24 +1,31 @@
 import pandas as pd
 data = pd.read_excel('Data_Train.xlsx')  # Read in the data we downloaded
 data.dropna(inplace=True)
-print("hello world")
+
 
 #Prints the first 5 values in the excel file
 print(data.head())
 
 airline_dict = {x:i for i, x in enumerate(data['Airline'].unique())}
+
 print(airline_dict)               
 def integer_encode_airline_dict(x):
   return airline_dict[x]                           
+  
 
 data['Airline']=data['Airline'].apply(integer_encode_airline_dict)
+
+print(data.head())
 
 
 source_dict = {y:i for i, y in enumerate(data['Source'].unique())}
 
+
 def integer_encode_source_dict(y):
   return source_dict[y]
+
 print(source_dict)
+
 data['Source']=data['Source'].apply(integer_encode_source_dict)
 
 destination_dict = {y:i for i, y in enumerate(data['Destination'].unique())}
@@ -43,10 +50,34 @@ print(data.head())
 data['Date_of_Journey'] = pd.to_datetime(data['Date_of_Journey'])
 
 data['day_of_week']=data['Date_of_Journey'].dt.day_name()
-data['Journey_Month']=data['Date_of_Journey'].dt.month_name()
+data['journey_month']=data['Date_of_Journey'].dt.month_name()
+
+print(data.head())
+
+day_dict = {x:i for i, x in enumerate(data['day_of_week'].unique())}
+
+def integer_encode_day_dict(x):
+    return day_dict[x]
+
+
+data['day_of_week']=data['day_of_week'].apply(integer_encode_day_dict)
+
+
+print(data.head())
 
 
 
 
+month_dict = {x:i for i, x in enumerate(data['journey_month'].unique())}
 
+def integer_encode_month_dict(x):
+    return month_dict[x]
+
+data['journey_month']=data['journey_month'].apply(integer_encode_month_dict)
+
+
+print(data.head())
+
+
+data['Route'].str.split(' ')
 print(data.head())
