6fc3c2a03bbd61c8228e5dff53da89dbc30baa2a
lopez-jose <lezep2@gmail.com>
2020-11-19 08:05:32 -0800

    added wget check for existence of file

--- linear_regression/linear_regression.py
+++ linear_regression/linear_regression.py
@@ -11,10 +11,14 @@
 
 #wget http://archive.ics.uci.edu/ml/machine-learning-databases/00368/Facebook_metrics.zip -O ./Facebook_metrics.zip
 
-
+import os
 import wget
 url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/00368/Facebook_metrics.zip'
-wget.download(url)
+PATH = './Facebook_metrics.zip'
+if os.path.isfile(PATH) and os.access(PATH,os.R_OK):
+    print("File exists and is readable")
+else:
+    wget.download(url)
 
 import zipfile
 with zipfile.ZipFile('./Facebook_metrics.zip', 'r') as zip_ref:
@@ -27,6 +31,7 @@ import numpy as np
 np.random.seed(144)
 
 
+
 def shuffle_data(data):
     np.random.shuffle(data)
 
@@ -34,3 +39,5 @@ def shuffle_data(data):
 lr_dataframe = pd.read_csv('dataset_Facebook.csv')
 lr_dataframe.dropna(inplace=True)
 print(lr_dataframe.head(10))
+
+
