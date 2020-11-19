3480647b5cbe5bc39a064f0103c1cf0643ac88ed
lopez-jose <lezep2@gmail.com>
2020-11-19 08:18:17 -0800

    added autoformatiing on save

--- linear_regression/linear_regression.py
+++ linear_regression/linear_regression.py
@@ -1,43 +1,42 @@
-#Jose Lopez
-#This code performs linear regression on data from scratch using python. 
+# Jose Lopez
+# This code performs linear regression on data from scratch using python.
 
-#this aims to map feature vectors to a continous value in the range [-infinity, +infinity]
-
-
-#Information on the data used can be seen in the following link
-
-#http://archive.ics.uci.edu/ml/datasets/Facebook+metrics
+# this aims to map feature vectors to a continous value in the range [-infinity, +infinity]
 
 
-#wget http://archive.ics.uci.edu/ml/machine-learning-databases/00368/Facebook_metrics.zip -O ./Facebook_metrics.zip
+# Information on the data used can be seen in the following link
 
+# http://archive.ics.uci.edu/ml/datasets/Facebook+metrics
+
+
+# wget http://archive.ics.uci.edu/ml/machine-learning-databases/00368/Facebook_metrics.zip -O ./Facebook_metrics.zip
+
+import numpy as np
+import pandas as pd
+import zipfile
 import os
 import wget
 url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/00368/Facebook_metrics.zip'
 PATH = './Facebook_metrics.zip'
-if os.path.isfile(PATH) and os.access(PATH,os.R_OK):
+if os.path.isfile(PATH) and os.access(PATH, os.R_OK):
     print("File exists and is readable")
 else:
     wget.download(url)
 
-import zipfile
 with zipfile.ZipFile('./Facebook_metrics.zip', 'r') as zip_ref:
     zip_ref.extractall('./')
 
 
-import pandas as pd
-import numpy as np
-
 np.random.seed(144)
-
 
 
 def shuffle_data(data):
     np.random.shuffle(data)
 
 
+columns_to_drop = ['Type', 'Lifetime Post Total Reach', 'Lifetime Post Total Impressions', 'Lifetime Engaged Users', 'Lifetime Post Consumers', 'Lifetime Post Consumptions',
+                   'Lifetime Post Impressions by people who have liked your page', 'Lifetime People who have liked your Page and engaged with your post', 'comment', 'like', 'share']
+
 lr_dataframe = pd.read_csv('dataset_Facebook.csv')
 lr_dataframe.dropna(inplace=True)
 print(lr_dataframe.head(10))
-
-
