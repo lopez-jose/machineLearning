b1e68236efd1ae0c281d3c7d742664a35adece48
Jose Lopez <lezep2@gmail.com>
2020-11-28 15:05:39 -0800

    added randomly generated data plot

--- non_linear_regression/non_linear_fit.py
+++ non_linear_regression/non_linear_fit.py
@@ -1,5 +1,5 @@
 import numpy as np
-import matplotlib.pyplot as pyplot
+import matplotlib.pyplot as plt
 
 
 def normalize_data(data):
@@ -9,4 +9,17 @@ def normalize_data(data):
 np.random.seed(33)
 x = np.random.uniform(-10, 10, 1000)
 poly_coeffs = np.random.uniform(-1, 1, size=(4, 1))
-y = poly_coeffs[0]+poly_coeffs[1]
+y = poly_coeffs[0]+poly_coeffs[1]*x + poly_coeffs[2] * \
+    (x**2)+poly_coeffs[3]*(x**3) + np.random.normal(0, 250, 1000)
+
+x2 = np.random.uniform(-10, 10, 1000)
+poly_coeffs = np.random.uniform(-1, 1, size=(4, 1))
+y2 = poly_coeffs[0]-2000+poly_coeffs[1]*x2+50 * \
+    poly_coeffs[2]*(x2**2)+np.random.normal(0, 250, 1000)
+x = np.concatenate([x, x2])
+y = np.concatenate([y, y2])
+x = normalize_data(x)
+y = normalize_data(y)
+
+plt.scatter(x, y, s=10)
+plt.show()
