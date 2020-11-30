a0832e80dde84caa1e033a511de07c802195b875
Jose Lopez <lezep2@gmail.com>
2020-11-29 21:16:16 -0800

    added necessary libraries to venv

---
+++ data_exploration/data_exploration.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+from tensorflow import keras
+
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+
+fashion_mnist = keras.datasets.fashion_mnist
+
+# splitting training and test data and corresponding labels
+
+(train_images, train_labels)
