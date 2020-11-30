9c5552ee6aa59171ed654a038e78b9c1bb0ba6d0
Jose Lopez <lezep2@gmail.com>
2020-11-29 21:37:49 -0800

    added show_image method for keras clothing files

--- data_exploration/data_exploration.py
+++ data_exploration/data_exploration.py
@@ -12,5 +12,26 @@ import pandas as pd
 fashion_mnist = keras.datasets.fashion_mnist
 
 # splitting training and test data and corresponding labels
+(train_images, train_labels), (test_images,
+                               test_labels) = fashion_mnist.load_data()
 
-(train_images, train_labels)
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress',
+               'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+
+# makes a dictionary for each
+class_dict = {i: class_name for i, class_name in enumerate(class_names)}
+
+print(class_dict)
+
+
+def show_image(index):
+    plt.figure()
+    plt.imshow(train_images[index], cmap=plt.cm.binary)
+    plt.title(class_names[train_labels[index]])
+    plt.colorbar()
+    plt.show()
+
+
+show_image(1)
